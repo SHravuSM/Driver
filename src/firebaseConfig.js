e1cff0259e164245588915c33699b45af7045c06
SHravuSM <SHravansm169@gmail.com>
2024-11-26 19:43:56 +0530

    testing

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBfhBth9cq9-QaYmoXeQeXqNFI1CH_VD7A",
-  authDomain: "vihar-70d82.firebaseapp.com",
-  projectId: "vihar-70d82",
-  storageBucket: "vihar-70d82.firebasestorage.app",
-  messagingSenderId: "270779880218",
-  appId: "1:270779880218:web:4a62f8fae9b7c087e252a3"
+  apiKey: "AIzaSyCGa9-VXOotA47mY1EIBCPPan7Npz5-LQ0",
+  authDomain: "vihar-enterprise.firebaseapp.com",
+  projectId: "vihar-enterprise",
+  storageBucket: "vihar-enterprise.firebasestorage.app",
+  messagingSenderId: "1020787252700",
+  appId: "1:1020787252700:web:69cb2b9790bcf3224ac319",
+  measurementId: "G-Q483GXCPPQ"
 };
 
 const app = initializeApp(firebaseConfig);
